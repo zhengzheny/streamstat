43fc0ff7c3c9461efe8d77c0609bed0bd33955bd
jasonfanjj <jasonfanjj@163.com>
2017-04-05 15:34:56 +0800

    add 5 counters
    add 5 counters

--- src/main/java/com/gsta/bigdata/stream/counter/DomainStat1HourCounter.java
+++ src/main/java/com/gsta/bigdata/stream/counter/DomainStat1HourCounter.java
@@ -1,56 +1,35 @@
 package com.gsta.bigdata.stream.counter;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.gsta.bigdata.stream.utils.ConfigSingleton;
-import com.gsta.bigdata.stream.utils.Constants;
 import com.gsta.bigdata.stream.utils.SysUtils;
-import com.gsta.bigdata.stream.utils.WindowTime;
 
 public class DomainStat1HourCounter extends AbstractCounter {
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	// flush time gap,flush counter result
-	private long flushTimeGap;
 
 	public DomainStat1HourCounter(String name) {
 		super(name);
-		double t = 1 * 3600 * 1000 * ConfigSingleton.getInstance()
-				.getCounterFlushTimeGapRatio(super.name);
-		this.flushTimeGap = (long) t;
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void add(String kafkaKey, Map<String, String> valueData, long timeStamp) {
-		if (kafkaKey == null || valueData == null) {
-			return;
-		}
-		String Domain = SysUtils.getLevel3Domain(valueData.get("Domain"));
-		WindowTime.WinTime winTime = WindowTime.get1hour(timeStamp);
-		String key = Domain + Constants.KEY_DELIMITER + winTime.getTimeStamp();;
-		logger.info("DomainStat:"+ key);
-		super.addCount(key);
-		super.addCountTimeStamp(key);
+		// TODO Auto-generated method stub
 
 	 }
 
 	@Override
 	public long getFlushTimeGap() {
-		return this.flushTimeGap;
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	public static void main(String[] args){
-		Map<String, String> valueData = new HashMap<String, String>();
-		valueData.put("Domain", "szextshort.weixin.qq.com");
-		String domain=valueData.get("Domain");
+		String domain="szextshort.weixin.qq.com";
 		//domain="api.weibo.cn";
 		//domain = "qq.com";
 		//domain="182.254.116.117:80";
 		//domain="182.254.116.117";
-		//domain = "amdc.m.taobao.com:8090";
+		domain = "amdc.m.taobao.com:8090";
 		
 		System.out.println(SysUtils.getLevel3Domain(domain));
 	}
